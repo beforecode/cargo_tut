58abaea8160ceb9ad2beb1b64b9880f5fcf25b2f
beforecode <yassineuniversite@gmail.com>
2022-05-28 16:28:32 +0100

    initial commit

---
+++ src/print.rs
@@ -0,0 +1,4 @@
+pub fn run() {
+	//print to console
+	println!(" {name} likes to {activity} ", name= "yassine", activity = "code");
+}
---
+++ src/vars.rs
@@ -0,0 +1,6 @@
+pub fn run() {
+	let name = "Mohammed";
+	let mut age = 23;
+	age = 24;
+	println!("my name is {} & my age is {}", name, age);
+}
