0b11b8e7289f10f5e986cde559a190f301e02758
beforecode <yassineuniversite@gmail.com>
2022-05-29 12:42:18 +0100

    data types and unicode

--- src/vars.rs
+++ src/vars.rs
@@ -1,6 +1,15 @@
 pub fn run() {
+	//mutable data
 	let name = "Mohammed";
-	let mut age = 23;
-	age = 24;
+	let age = 23;
+	
 	println!("my name is {} & my age is {}", name, age);
+
+	//const keyword
+	const ID: i32 = 001; //ID variable is a 32 bit type of data
+	println!("The id is {}", ID);
+
+	//defining multiple varibles
+	let (pc_name, pc_price) = ("DELL", 150);
+	println!("The {} cost {}", pc_name, pc_price);
 }
